daf1edf75441d6318628e48e36394de6b07440fd
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-05-05 11:00:17 +0545

    change eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
-
+import React from "react";
 
 function App() {
   return (
     <div className="h-screen bg-blue-300 flex justify-center item-center">
-      <button className="bg-green-200 h-10 p-4 rounded-3xl ">Dark Mode</button>
+      
     </div>
   );
 }

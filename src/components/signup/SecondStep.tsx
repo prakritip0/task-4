5bafc1412f4bd467e3844b737dac0f1b4d8af0b4
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-05-30 09:21:17 +0545

    complete validation for second step

--- src/components/signup/SecondStep.tsx
+++ src/components/signup/SecondStep.tsx
@@ -14,7 +14,6 @@ const SecondStep = () => {
   const [skillTags, setSkillTags] = useState<string[]>([]);
   const [secondNextDisabled, setSecondNextDisabled] = useState(true);
   const [degreeTypeErr, setDegreeTypeErr] = useState('');
-  
 
   const { moveForward, goBackward } = useOutletContext<{
     moveForward: () => void;
@@ -34,11 +33,16 @@ const SecondStep = () => {
     formalDegree ? setFormalDegreeErr('') : setFormalDegreeErr('Formal Degree is required.');
   };
   const validateSkill = () => {
-    skill ? setSkillErr('') : setSkillErr('Skills are required.');
+    if(skillTags.length===0){
+      skill ? setSkillErr('') : setSkillErr('Skills are required.');
+    }else{
+      setSkillErr('')
+    }
+    
   };
-const validateDegreeType=()=>{
-  degreeType? setDegreeTypeErr(''): setDegreeTypeErr("Please mention your degree's name.")
-}
+  const validateDegreeType = () => {
+    degreeType ? setDegreeTypeErr('') : setDegreeTypeErr("Please mention your degree's name.");
+  };
   const removeTag = (i: number) => {
     const updatedSkills = skillTags;
     updatedSkills.splice(i, 1);
@@ -48,15 +52,19 @@ const validateDegreeType=()=>{
   // console.log('skillz', skillTags);
 
   useEffect(() => {
+    console.log(skillTags.length <= 0);
+
     const noErrMessage = !skillErr && !formalDegreeErr && !degreeTypeErr;
-    const isDisabled = !skill || !formalDegree || !noErrMessage;
+    const isDisabled = skillTags.length <= 0 || !formalDegree || !degreeType || !noErrMessage;
+    console.log('noerr',noErrMessage);
+    
     setSecondNextDisabled(isDisabled);
-  }, [skill, formalDegree, skillErr, formalDegreeErr, degreeTypeErr]);
+  }, [skillTags, formalDegree, skillErr,degreeType, formalDegreeErr, degreeTypeErr]);
 
   return (
     <div className='flex flex-col h-full align-start justify-between w-[30rem] mx-[7rem] pt-[6rem]'>
       <div className='flex flex-col items-start  gap-6'>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-1 w-full'>
           <Input
             id='skills'
             value={skill}
@@ -88,25 +96,24 @@ const validateDegreeType=()=>{
             { value: 'masters', label: 'Bachelors' },
             { value: 'bachelors', label: 'Masters' },
             { value: 'highschool', label: 'High School' },
-            { value: 'none', label: 'None' },
+            { value: 'other', label: 'Other' },
           ]}
           onBlur={validateFormalDegree}
           errMessage={formalDegreeErr}
           labelName='Formal Degree'
         />
-        {formalDegree && formalDegree !== 'none' && (
-          <Input
-            id='degreeType'
-            value={degreeType}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setDegreeType(e.target.value)}
-            placeholder='Formal Degree Name'
-            labelName='Degree Name'
-            type='text'
-            className='w-[20rem]'
-            onBlur={validateDegreeType}
-            error={degreeTypeErr}
-          />
-        )}
+
+        <Input
+          id='degreeType'
+          value={degreeType}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setDegreeType(e.target.value)}
+          placeholder='Formal Degree Name'
+          labelName='Degree Name'
+          type='text'
+          onBlur={validateDegreeType}
+          error={degreeTypeErr}
+          disabled={formalDegree ? false : true}
+        />
       </div>
       <div className='flex gap-6 pt-[2rem] 2xl:pt-[4rem]'>
         <Link to='/signup'>

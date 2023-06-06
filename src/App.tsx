e0d2a3a9630f76ffec07c0bd9f584d61118b2dab
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-06-06 09:18:46 +0545

    create a router file and render it inside app

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,8 @@
 import React from 'react';
-import Main from './Main';
-import ErrorPage from './pages/ErrorPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Banner from './components/Banner';
-import SignUp from './pages/SignUp';
-import JobVacancy from './pages/JobVacancy';
-import AboutUs from './pages/AboutUs';
-import Pricing from './pages/Pricing';
-import FirstStep from './components/signup/FirstStep';
-import SecondStep from './components/signup/SecondStep';
-import ThirdStep from './components/signup/ThirdStep';
-import FinalStep from './components/signup/FinalStep';
-import FormSubmitted from './components/signup/FormSubmitted';
+import Router from './Router';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/task-4',
-      element: <Main />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: '/',
-          element: <Banner />,
-        },
-        {
-          path: '/signup',
-          element: <SignUp />,
-          children: [
-            { path: '/signup', element: <FirstStep /> },
-            { path: '/signup/second-step', element: <SecondStep /> },
-            { path: '/signup/third-step', element: <ThirdStep /> },
-            { path: '/signup/final-step', element: <FinalStep /> },
-            { path: '/signup/submitted', element: <FormSubmitted /> },
-          ],
-        },
-        {
-          path: '/jobvacancy',
-          element: <JobVacancy />,
-        },
-        {
-          path: '/aboutus',
-          element: <AboutUs />,
-        },
-        {
-          path: '/pricing',
-          element: <Pricing />,
-        },
-      ],
-    },
-  ],{basename: "/task-4"});
-  return (
-    // <Main/>
-    <RouterProvider router={router} />
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Banner from './components/Banner';
+import FinalStep from './components/signup/FinalStep';
+import FirstStep from './components/signup/FirstStep';
+import FormSubmitted from './components/signup/FormSubmitted';
+import SecondStep from './components/signup/SecondStep';
+import ThirdStep from './components/signup/ThirdStep';
+import Main from './Main';
+import AboutUs from './pages/AboutUs';
+import ErrorPage from './pages/ErrorPage';
+import JobVacancy from './pages/JobVacancy';
+import Pricing from './pages/Pricing';
+import SignUp from './pages/SignUp';
+
+const Router = () => {
+  
+    const router = createBrowserRouter([
+        {
+          path: '/',
+          element: <Main />,
+          errorElement: <ErrorPage />,
+          children: [
+            {
+              path: '/',
+              element: <Banner />,
+            },
+            {
+              path: '/signup',
+              element: <SignUp />,
+              children: [
+                { path: '/signup', element: <FirstStep /> },
+                { path: '/signup/second-step', element: <SecondStep /> },
+                { path: '/signup/third-step', element: <ThirdStep /> },
+                { path: '/signup/final-step', element: <FinalStep /> },
+                { path: '/signup/submitted', element: <FormSubmitted /> },
+              ],
+            },
+            {
+              path: '/jobvacancy',
+              element: <JobVacancy />,
+            },
+            {
+              path: '/aboutus',
+              element: <AboutUs />,
+            },
+            {
+              path: '/pricing',
+              element: <Pricing />,
+            },
+          ],
+        },
+      ]);
+      return (
+        <RouterProvider router={router} />
+      );
+    }
+    
+
+export default Router
